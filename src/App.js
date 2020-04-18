08ee913ba7258d46cda36efd49edb7d829016675
Muhammad Anas <muhd.anas@outlook.com>
2020-04-18 11:50:10 +0800

    Boilerplate setup for redux.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import SearchBar from './components/layout/SearchBar';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 import Logs from './components/logs/Logs';
 
 const App = () => {
@@ -15,16 +19,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <div>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <Logs />
-      </div>
-    </div>
+    <Provider store={store}>
+      <>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <Logs />
+          <TechListModal />
+        </div>
+      </>
+    </Provider>
   );
 };
 
