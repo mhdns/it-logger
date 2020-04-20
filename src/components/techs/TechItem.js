2668b01b575c9736bc2dba05185fa18142f3e8b2
Muhammad Anas <muhd.anas@outlook.com>
2020-04-20 10:13:38 +0800

    Implemented delete tech. Project Completed.

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,14 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteTech } from '../../actions/techActions';
 
-const TechItem = ({ tech }) => (
+const TechItem = ({ tech, deleteTech }) => (
   <li className="collection-item">
     <div>
       {`${tech.firstName} ${tech.lastName}`}
-      <a href="#!" className="secondary-content">
+      <a href="#!" className="secondary-content" onClick={() => deleteTech(tech.id)}>
         <i className="material-icons grey-text">delete</i>
       </a>
     </div>
@@ -14,7 +16,8 @@ const TechItem = ({ tech }) => (
 );
 
 TechItem.propTypes = {
-  tech: PropTypes.object.isRequired
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired
 };
 
-export default TechItem;
+export default connect(null, { deleteTech })(TechItem);
